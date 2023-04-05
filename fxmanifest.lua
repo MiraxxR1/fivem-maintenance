caa5d16d698c969fb1f31b42eaf30bde1ad2a430
MiraxxR <75571297+MiraxxR1@users.noreply.github.com>
2023-04-05 19:12:12 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,15 @@ fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
+name 'fivem-maintenance'
+author 'MiraxxR'
+contact 'MiraxxR#8801'
+repository 'https://github.com/MiraxxR1/fivem-maintenance'
+discord 'https://discord.gg/wJmnKQHTWD'
+description 'This Lua script for FiveM allows you to set up a maintenance system for your server. '
+
 server_scripts {
     "server/sv_maintenance.lua"
 }
+
+print("^1 fivem-maintenance - By MiraxxR#8801 - https://github.com/MiraxxR1/fivem-maintenance - https://discord.gg/wJmnKQHTWD") 
